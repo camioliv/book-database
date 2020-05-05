1fdbc17d040c7933b5a09c9fe1d8d8036ccaed48
Matthew Ary <matthewj.c.ary@gmail.com>
2020-05-05 07:34:32 -0700

    Fix book search function

--- apirest/src/main/java/com/books/apirest/repository/BookRepository.java
+++ apirest/src/main/java/com/books/apirest/repository/BookRepository.java
@@ -9,9 +9,11 @@ import com.books.apirest.models.Book;
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 	Book findById(long id);
-	
-	List<Book> findAllByTitleContainingOrIsbnEqualsOrAuthorsNameContainingOrTopicsEquals(String title, String isbn, String author, String topic);
-	List<Book> findAllByPagesIsLessThan(int pages);	
+
+	// While you made the strings you send to this method all lowercase, the query is still going to be
+	// case sensitive unless you specify for the query to ignore the case sensitivity.
+	List<Book> findAllByTitleContainingIgnoreCaseOrIsbnEqualsOrAuthorsNameContainingIgnoreCaseOrTopicsEquals(String title, String isbn, String author, String topic);
+	List<Book> findAllByPagesIsLessThan(int pages);
 	List<Book> findAllByRatingsStarsAndRatingsUserId(int stars, long userId);
 	List<Book> findTop10ByOrderByLastViewdDesc();
 }
